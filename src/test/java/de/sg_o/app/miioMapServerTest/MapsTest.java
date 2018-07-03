968fe7e77e7f0b769ba20e700b003605b016f53f
SG-O <admin@sg-o.de>
2018-07-03 02:13:33 +0200

    Fixed MapsTest

--- src/test/java/de/sg_o/app/miioMapServerTest/MapsTest.java
+++ src/test/java/de/sg_o/app/miioMapServerTest/MapsTest.java
@@ -58,7 +58,7 @@ public class MapsTest {
 
     @Test
     public void previousTest() {
-        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4598, boundingBox=java.awt.Rectangle[x=460,y=409,width=90,height=118], overSample=1}", s0.getLastMap().toString());
+        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4598, boundingBox=[460, 409, 90, 118], overSample=1}", s0.getLastMap().toString());
     }
 
     @Test
@@ -66,8 +66,8 @@ public class MapsTest {
         assertEquals(2, s0.numberOfPreviousMaps());
         assertTrue(s0.getPreviousMaps().contains("000143.20180604001001609_1387101062713_2018032100REL"));
         assertTrue(s0.getPreviousMaps().contains("000144.20180604034309095_1387101062713_2018032100REL"));
-        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4831, boundingBox=java.awt.Rectangle[x=442,y=450,width=133,height=117], overSample=1}", s0.getOldMap("000143.20180604001001609_1387101062713_2018032100REL").toString());
-        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4598, boundingBox=java.awt.Rectangle[x=460,y=409,width=90,height=118], overSample=1}", s0.getOldMap("000144.20180604034309095_1387101062713_2018032100REL").toString());
+        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4831, boundingBox=[442, 450, 133, 117], overSample=1}", s0.getOldMap("000143.20180604001001609_1387101062713_2018032100REL").toString());
+        assertEquals("de.sg_o.app.miioMapServer.VacuumMap{map=width:1024; height:1024, pathEntries=4598, boundingBox=[460, 409, 90, 118], overSample=1}", s0.getOldMap("000144.20180604034309095_1387101062713_2018032100REL").toString());
         s0.updatePreviousMaps();
         assertEquals(2, s0.numberOfPreviousMaps());
         assertEquals(2, s1.numberOfPreviousMaps());
